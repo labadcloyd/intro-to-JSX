368dd42d80c9335d79068e8f446b0d9bc7794dda
Cloyd Abad <67862525+labadcloyd@users.noreply.github.com>
2021-03-05 10:13:16 +0800

    added conditions to an element

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-let topic = "food";
+let topic = "Here are my favorite foods:";
 let favFoods = ["French Fries", "Chicken Breasts", "Burger"];
-var d = new Date();
-var currentYear = d.getFullYear();
+let d = new Date();
+let currentYear = d.getFullYear();
+let greeting = "Hello!";
+let greetingStyle = {
+  fontSize: "1.7rem",
+  color: "#000000"
+};
+let currentHour = new Date().getHours();
+console.log(currentHour);
+
+if (currentHour > 7 && currentHour <= 10) {
+  greeting = "Good Morning";
+  greetingStyle = {
+    color: "#3295a8"
+  };
+} else if (currentHour > 10 && currentHour <= 12) {
+  greeting = "Good Noon";
+  greetingStyle = {
+    color: "#36a832"
+  };
+} else if (currentHour > 12 && currentHour <= 18) {
+  greeting = "Good Afternoon";
+  greetingStyle = {
+    color: "#a88932"
+  };
+} else if (currentHour > 18 && currentHour <= 24) {
+  greeting = "Good Night";
+  greetingStyle = {
+    color: "#4232a8"
+  };
+}
 
 ReactDOM.render(
   <div>
-    <h1 contentEditable="true">{topic}</h1>
+    <h1 style={greetingStyle}>{greeting}</h1>
+    <h2>{topic}</h2>
     <div>
       <img
         className="image"
