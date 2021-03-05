97afd77cc177cc216433b1d489fbcd2483a537ad
Cloyd Abad <67862525+labadcloyd@users.noreply.github.com>
2021-03-05 20:47:56 +0800

    Tried adding multiple modules

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Greeting from "./greeting";
 import Images from "./images";
+import { Add, Minus, Divide, Multiply } from "./math";
 
 let topic = "Here are my favorite foods:";
 let d = new Date();
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Greeting />
     <h2>{topic}</h2>
     <Images />
+    <p>{Add(3, 4)}</p>
     <hr></hr>
     <p>Copyright {currentYear}</p>
   </div>,
---
+++ src/math.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Add(a, b) {
+  return a + b;
+}
+function Minus(a, b) {
+  return a - b;
+}
+function Divide(a, b) {
+  return a / b;
+}
+function Multiply(a, b) {
+  return a * b;
+}
+export { Add, Minus, Divide, Multiply };
