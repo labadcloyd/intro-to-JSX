21bbcca939a1b737f525958da04339a76d4f399c
Cloyd Abad <67862525+labadcloyd@users.noreply.github.com>
2021-03-10 10:55:08 +0800

    shortened the card component code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,30 +2,18 @@ import React from "react";
 import Avatar from "./Avatar";
 import Detail from "./Detail";
 
-function Card(props) {
+function CreateNewCard(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <Avatar img={props.img} />
+        <Avatar img={props.imgURL} />
       </div>
       <div className="bottom">
-        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.phone} />
         <Detail detailInfo={props.email} />
       </div>
     </div>
   );
 }
-function createNewCard(contact) {
-  return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.phone}
-      email={contact.email}
-    />
-  );
-}
-export default Card;
-export { createNewCard };
+export default CreateNewCard;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import Images from "./components/images";
 import { Add, Minus, Divide, Multiply } from "./components/math";
 import User from "./components/user";
 import contacts from "./contacts";
-import Card from "./components/Card";
-import { createNewCard } from "./components/Card";
+import CreateNewCard from "./components/Card";
 
 let topic = "Here are my favorite foods:";
 let d = new Date();
@@ -16,8 +15,14 @@ ReactDOM.render(
   <div>
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map(createNewCard)}
+      {contacts.map(CreateNewCard)}
     </div>
+    <CreateNewCard
+      name="cloyd"
+      phone="123"
+      email="123@123.com"
+      imgURL="https://avatars.githubusercontent.com/u/67862525?s=460&u=24cc823790da24441df311ab96cfbbd3cb972ed0&v=4"
+    />
     <Greeting />
     <h2>{topic}</h2>
     <Images />
